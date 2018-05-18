c2faa3012301222c4a0928a8e4c3b74c4492696d
Axel <axelcoudou@free.fr>
2018-05-18 16:41:01 +0200

    Added clientside base for building the app

---
+++ client/components/Container.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class Container extends React.Component {
+    
+    render() {
+        return (
+            <div>
+                <h1 className="pouet">Nous sommes dans le container</h1>
+            </div>
+        )
+    }
+}
