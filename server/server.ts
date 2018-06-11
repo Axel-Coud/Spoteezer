1de4a88c7ba3740e9fcb7bd71a51e92ce33968d1
Axel <axelcoudou@free.fr>
2018-06-11 12:25:51 +0200

    Now serve the client

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,15 @@
 import express from 'express'
 import config from './config'
+import path from 'path'
 
 const server = express()
+
+server.use(express.static(__dirname + './../client/'))
+
+server.get('/', (_, res) => {
+
+    res.sendFile(path.join(__dirname, '../client/index.html'))
+})
 
 server.listen(config.port, () => {
     console.log(`/************************************\\
