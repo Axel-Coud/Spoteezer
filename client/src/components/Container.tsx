02b2056b04f6f94944297986f4846f7713bfe1e1
Axel <axelcoudou@free.fr>
2018-06-12 14:21:44 +0200

    Tested antd library with success

--- client/src/components/Container.tsx
+++ client/src/components/Container.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import {Button} from 'antd'
 
 export default class Container extends React.Component {
-    
+
     render() {
         return (
             <div>
                 <h1 className="pouet">Nous sommes dans le container</h1>
+                <Button type="primary" >Button</Button>
             </div>
         )
     }
