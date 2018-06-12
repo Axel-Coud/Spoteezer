971f6844394b5675d5525a8233b4395806fb120f
Axel <axelcoudou@free.fr>
2018-06-12 17:18:45 +0200

    Added commented server close catcher, but commented them for further tests (no utilities right now)

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,16 @@
 import express from 'express'
 import config from './config'
 import path from 'path'
+import router from './router/routerApex'
+import pgClient from './db'
 
 const server = express()
 
 // On sert les assets depuis le rootDir ./client
 server.use(express.static(__dirname + './../client/'))
+
+server.use(express.json())
+server.use(router)
 
 // Entry-point
 server.get('/', (_, res) => {
@@ -19,4 +24,23 @@ server.listen(config.port, () => {
   Server is now running on port ${config.port}
 
 /************************************\\`)
+})
+
+// le client se ferme déjà automatiquement(j'ai laissé si jamais j'en aurai besoin un jour)
+process.on('SIGINT', () => {
+    // pgClient.end((err) => {
+    //     if (err) {
+    //         console.log(err)
+    //     }
+        console.log('Server closed')
+//     })
+})
+
+process.on('exit' , async () => {
+//     pgClient.end((err) => {
+//         if (err) {
+//             console.log(err)
+//         }
+        console.log('Server closed')
+//     })
 })
