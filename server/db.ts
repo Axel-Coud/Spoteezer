adcfac2f582323d9d5734d8ea3d3a8557c86909c
Axel <axelcoudou@free.fr>
2018-06-12 16:12:01 +0200

    Added db client

---
+++ server/db.ts
@@ -0,0 +1,15 @@
+import { Client } from 'pg'
+
+const client = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'spoteezer',
+    password: 'admin',
+    port: 5432
+})
+
+client.on('error', (err) => {
+    console.log('Erreur émise par le client postgres: ', err)
+})
+
+export default client
