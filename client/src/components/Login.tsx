156f3e11f0e801151bd5de2010a8a5ac6285cbc1
Axel <axelcoudou@free.fr>
2018-06-15 12:01:30 +0200

    Connexion page done

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -5,37 +5,40 @@ export default class Login extends React.Component {
 
     public render() {
         const buttonStyle: CSSProperties = {
-            margin: 10
+            marginTop: 10
         }
 
         return(
-            <div style={{textAlign: "center"}}>
-                <Row type="flex" justify="center">
-                    <Col span={8}>
-                        <Form className="spz-login-form">
-                            <Form.Item>
-                                <Input
-                                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }}/>}
-                                    placeholder="Username" 
-                                />
-                            </Form.Item>
-                            <Form.Item>
-                                <Input
-                                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }}/>}
-                                    type="password"
-                                    placeholder="password"
-                                />
-                            </Form.Item>
-                            <Form.Item>
-                                <Checkbox>Se souvenir de moi ?</Checkbox><br/>
-                                <a className="spz-login-forgot" href="#">Mot de passe oublié</a><br/>
-                                <Button style={buttonStyle} size="large" type="primary" >Se connecter</Button><br/>
-                                <Button style={buttonStyle} size="large" type="dashed">Créer un compte</Button>
-                            </Form.Item>
-                        </Form>
-                    </Col>
-                </Row>
-            </div>
+            <Row className="spz-login-page" type="flex" justify="center">
+                <Col xs={20} sm={18} md={14} lg ={10}>
+                    <Form className="spz-login-form">
+                        <img
+                            src="./images/spoteezerIcon.png"
+                            alt="icône du site"
+                            style={{width: "80px", height: "80px", marginBottom: 10}}
+                        />
+                        <Form.Item>
+                            <Input
+                                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }}/>}
+                                placeholder="Username"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Input
+                                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }}/>}
+                                type="password"
+                                placeholder="password"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Checkbox>Se souvenir de moi</Checkbox>
+                            <a className="spz-login-forgot" href="#">Mot de passe oublié</a><br/>
+                            <Button style={buttonStyle} size="large" type="primary" >Se connecter</Button><br/>
+                            <Button style={buttonStyle} size="large" type="dashed">Créer un compte</Button>
+                        </Form.Item>
+                    </Form>
+                </Col>
+            </Row>
         )
     }
 }
