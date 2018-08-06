942390686cdbb5985a885682ac879bc1b0bdff9e
Axel <axelcoudou@free.fr>
2018-08-06 16:27:24 +0200

    Added user firstname information

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Layout, Menu, Breadcrumb, Icon } from 'antd'
+import {GlobalContext} from '../global/Global'
 
 const { Header, Content, Footer, Sider } = Layout
 const SubMenu = Menu.SubMenu
@@ -53,8 +54,12 @@ export default class Home extends React.Component {
                 <Header style={{ background: '#fff', padding: 0 }} />
                 <Content style={{ margin: '0 16px' }}>
                     <Breadcrumb style={{ margin: '16px 0' }}>
-                        <Breadcrumb.Item>User</Breadcrumb.Item>
-                        <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                        <Breadcrumb.Item>Utilisateur</Breadcrumb.Item>
+                        <Breadcrumb.Item>
+                            <GlobalContext.Consumer>
+                                {(global) => global.actions.getCurrentUser()!.uti_prenom}
+                            </GlobalContext.Consumer>
+                        </Breadcrumb.Item>
                     </Breadcrumb>
                     <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
                         <h1>( ͡° ͜ʖ ͡°)</h1>
