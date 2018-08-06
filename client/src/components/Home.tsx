b124d57c8724651af4c1c1920dbb6ffafb184d5d
Axel <axelcoudou@free.fr>
2018-08-06 16:27:51 +0200

    Added logo to home sider

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -23,7 +23,7 @@ export default class Home extends React.Component {
                 collapsed={this.state.collapsed}
                 onCollapse={this.onCollapse}
             >
-                <div className="logo" />
+                <img src="./images/spoteezerIcon.png" className="home-sider-logo" />
                 <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
                     <Menu.Item key="1">
                         <Icon type="caret-right" className="spz-home-layout-icon" />
