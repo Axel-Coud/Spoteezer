05eb5c4d12658d6dcc2400431d2a38a7fe2cdfd8
Axel <axelcoudou@free.fr>
2018-09-04 15:13:26 +0200

    added router music

--- server/router/routerApex.ts
+++ server/router/routerApex.ts
@@ -1,11 +1,13 @@
 import {Router} from 'express'
 import routerUsers from './routerUsers'
 import routerSession from './routerSession'
+import routerMusics from './routerMusics'
 
 // On link tout nos router ici et on envoit pour pas bloat 'server.ts'
 const routerApex = Router()
 
 routerApex.use('/users', routerUsers)
+routerApex.use('/musics', routerMusics)
 routerApex.use(routerSession)
 
 export default routerApex
