bb2f42b4e4c9aee5b589bd1cca98d4a2698c47a3
Axel <axelcoudou@free.fr>
2018-10-01 16:15:44 +0200

    Added table to display musics

--- client/src/components/content/Musique.tsx
+++ client/src/components/content/Musique.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
 import { Music } from '../../../../server/controller/musics/getAllMusic'
+import { Table, Icon, Divider, notification } from 'antd'
+import { GlobalActions } from '../../global/Global'
+import { ColumnProps } from 'antd/lib/table'
 
 interface Props {
-    uti_id: number
+    globalActions: GlobalActions
 }
 
 interface State {
@@ -16,27 +19,73 @@ export default class Musique extends React.Component<Props, State> {
     }
 
     async componentDidMount() {
-        const musicList = await axios({
-            method: 'get',
-            url: 'http://localhost:8888/musics/all'
-        })
-        console.log(this.props)
 
-        this.setState({ musicList: musicList.data })
+        let musicList: null | AxiosResponse<any> = null
+        try {
+            await this.props.globalActions.verifyCurrentUser()
+            musicList = await axios({
+                method: 'get',
+                url: 'http://localhost:8889/musics/all'
+            })
+        } catch (error) {
+
+            notification.error({
+                message: "Erreur",
+                description: error.message,
+                duration: 2
+            })
+            return
+        }
+
+        // Ce map est uniquement là pour mettre des prorpiétés 'key' par conventions de antd à chaque data
+        const test = musicList.data.map((item, index) => {
+            return { ...item, key: index }
+        })
+
+        this.setState({ musicList: test })
     }
 
     render() {
-        return (
-            <div style={{ textAlign: 'center' }}>
-                {this.state.musicList.map((music, index) => {
-                    return (
-                        <div key={index}>
+        const columns: ColumnProps<Music>[] = [{
+            title: '',
+            key: 'play',
+            render: () => {
+                return (<span>
+                    <Icon type="play-circle" />
+                </span>)
 
-                            <span>{music.title} | {music.artist} | {music.duration}</span>
-                        </div>
-                    )
-                })}
-            </div>
-        )
+            }
+        }, {
+            title: 'Titre',
+            dataIndex: 'title',
+            key: 'title'
+        }, {
+            title: 'Artiste',
+            dataIndex: 'artist',
+            key: 'artist'
+        }, {
+            title: <Icon type='clock-circle' />,
+            dataIndex: 'duration',
+            key: 'duration'
+        }, {
+            title: 'Actions',
+            key: 'actions',
+            render: (_, record) => {
+
+            const deleteElement =
+                (<span>
+                    <Divider type="vertical" />
+                    <a onClick={this.placeholder}><Icon type="close" /></a>
+                </span>)
+
+            return (
+                <span>
+                    <a onClick={this.doShit}><Icon type="download" /></a>
+                    {this.props.globalActions.getCurrentUser()!.uti_id ? deleteElement : ''}
+                </span>)
+            }
+        }]
+
+        return <Table columns={columns} dataSource={this.state.musicList} />
     }
 }
