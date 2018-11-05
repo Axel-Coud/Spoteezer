318c7ffad38ef5d45f3ec276bfd33ad3f4943243
Axel <axelcoudou@free.fr>
2018-11-05 12:21:44 +0100

    Added UserId to authenticated routes

--- server/Authentication.ts
+++ server/Authentication.ts
@@ -1,7 +1,16 @@
 import jwt from 'jsonwebtoken'
 import { Request, NextFunction, Response } from 'express'
 
-export default async function validateToken(req: Request, res: Response, next: NextFunction): Promise<void> {
+// Le fait d'être authentifié nous permet d'avoir l'userId dans la requête
+declare global {
+    namespace Express {
+        interface Request {
+            userId?: number
+        }
+    }
+}
+
+export default async function validateToken(req: Request , res: Response, next: NextFunction): Promise<void> {
 
     // Si l'utilisateur veut créer un compte ou se connecter on accepte sans token
     if (req.originalUrl === '/users/add' || req.originalUrl === '/login' || req.originalUrl === '/auth') {
@@ -14,13 +23,16 @@ export default async function validateToken(req: Request, res: Response, next: N
         const { token } = req.cookies
         // @ts-ignore process.env.JWT_SECRET should not be considered undefined in any circumstances
         const secret: string = process.env.JWT_SECRET
-        jwt.verify(token, secret, (err) => {
+        jwt.verify(token, secret, (err, decoded) => {
             // Si le token n'est pas authentifié on renvoit sur l'entry point du serveur
             if (err) {
                 console.log("JSON web token invalide pour ", req.originalUrl, " : " , err)
                 return res.redirect('/')
             }
             console.log('/--/Token validé pour la route : ' + req.originalUrl)
+
+            // On stock le userId pour la requête(moyen d'accès plus simple)
+            req.userId = decoded.userId
             next()
         })
 
@@ -36,8 +48,7 @@ export const generateToken = (userId: number): string => {
     if (process.env.JWT_SECRET) {
         const secret: string = process.env.JWT_SECRET
         const payload: JwtPayload = {
-            userId,
-            data: 'pouet'
+            userId
         }
         const token = jwt.sign(payload, secret, {
             expiresIn: "30m"
