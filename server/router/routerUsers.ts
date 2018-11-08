b21fad89902b89c038cf1461b5258f3bf47fb7f4
Axel <axelcoudou@free.fr>
2018-11-08 17:02:55 +0100

    Removed useless route

--- server/router/routerUsers.ts
+++ server/router/routerUsers.ts
@@ -1,21 +1,7 @@
 import { Router } from 'express'
 import addUser, { User } from '../controller/users/addUser'
-import getAllUsers from '../controller/users/getAllUsers'
 
 const router = Router()
-
-router.get('/all', async (_, res) => {
-
-    let users: null | User[] = null
-    try {
-        users = await getAllUsers()
-    } catch (error) {
-        console.log('Erreur dans get all users :' + error)
-        return res.status(500).send(error.message)
-    }
-
-    return res.status(200).send(users)
-})
 
 router.post('/add', async (req, res) => {
 
